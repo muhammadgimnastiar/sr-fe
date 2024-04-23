c4edc4c1a6e98d78752fd940171dea063c77705e
Muhmmad Gimnastiar <adagimnas@gmail.com>
2024-04-23 08:04:03 +0700

    update login verification

--- src/pages/public/auth/SignUpPage.jsx
+++ src/pages/public/auth/SignUpPage.jsx
@@ -10,7 +10,14 @@ import { Link, useNavigate } from "react-router-dom";
 const SignupSchema = Yup.object().shape({
   name: Yup.string().required("Nama diperlukan"),
   email: Yup.string().required("Email diperlukan"),
-  password: Yup.string().required("Password diperlukan"),
+  password: Yup.string()
+    .required("Password diperlukan")
+    .min(8, "Password must be at least 8 characters long")
+    .max(16, "Password must be at max 16 characters long")
+    .matches(
+      /^(?=.*[A-Z])(?=.*[0-9])/,
+      "Password must contain at least one capital letter and number"
+    ),
 });
 
 const defaultValues = {
@@ -29,13 +36,19 @@ export default function SignUpPage() {
     defaultValues,
   });
 
-  const { handleSubmit } = methods;
+  const {
+    handleSubmit,
+    formState: { errors },
+  } = methods;
 
   const onSubmit = async (data) => {
     setButtonLoading(true);
     signupMutation({ data })
       .then(() => {
         navigate("/signin");
+      })
+      .catch((error) => {
+        console.log(error);
       })
       .finally(() => {
         setButtonLoading(false);
@@ -49,25 +62,72 @@ export default function SignUpPage() {
           <div className="flex flex-col justify-center">
             <div className="flex flex-row gap-5 items-center xl:justify-start lg:justify-start md:justify-center sm:justify-center max-[640px]:justify-center">
               <img src="/logo.png" alt="Logo" width={40} height={40} />
-              <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-textColor md:text-5xl lg:text-6xl">Join Us!</h1>
+              <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-textColor md:text-5xl lg:text-6xl">
+                Join Us!
+              </h1>
             </div>
-            <p className="mb-4 mt-2 text-lg font-normal text-gray-500 lg:text-xl xl:text-start lg:text-start md:text-center sm:text-center max-[640px]:text-center">Please register your account first</p>
-            <Link to={"/"} className="text-textColor hover:underline font-medium text-lg inline-flex items-center xl:justify-start lg:justify-start md:justify-center sm:justify-center max-[640px]:justify-center">
+            <p className="mb-4 mt-2 text-lg font-normal text-gray-500 lg:text-xl xl:text-start lg:text-start md:text-center sm:text-center max-[640px]:text-center">
+              Please register your account first
+            </p>
+            <Link
+              to={"/"}
+              className="text-textColor hover:underline font-medium text-lg inline-flex items-center xl:justify-start lg:justify-start md:justify-center sm:justify-center max-[640px]:justify-center"
+            >
               Back to Home
-              <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+              <svg
+                className="w-3.5 h-3.5 ml-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
               </svg>
             </Link>
           </div>
           <div>
             <div className="w-full lg:max-w-xl p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow-xl">
-              <h2 className="text-2xl font-bold text-textColor">Create Your Account</h2>
+              <h2 className="text-2xl font-bold text-textColor">
+                Create Your Account
+              </h2>
               <RHFProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-                <RHFTextField name="name" label="Your name" type="text" helperText="username" />
-                <RHFTextField name="email" label="Your email" type="email" helperText="name@gmail.com" />
-                <RHFTextField name="password" label="Your password" type="password" helperText="••••••••" />
-                <button type="submit" className="w-full px-5 py-3 text-base font-medium text-center bg-primary hover:bg-green-700 text-white rounded-lg" disabled={buttonLoading}>
-                  {buttonLoading ? "Create your account..." : "Sign up your account"}
+                <RHFTextField
+                  name="name"
+                  label="Your name"
+                  type="text"
+                  helperText="username"
+                />
+                <RHFTextField
+                  name="email"
+                  label="Your email"
+                  type="email"
+                  helperText="name@gmail.com"
+                />
+                <RHFTextField
+                  name="password"
+                  label="Your password"
+                  type="password"
+                  helperText="••••••••"
+                />
+                {errors.password && (
+                  <span className="text-red-500 text-sm">
+                    {errors.password.message}
+                  </span>
+                )}
+                <button
+                  type="submit"
+                  className="w-full px-5 py-3 text-base font-medium text-center bg-primary hover:bg-green-700 text-white rounded-lg"
+                  disabled={buttonLoading}
+                >
+                  {buttonLoading
+                    ? "Create your account..."
+                    : "Sign up your account"}
                 </button>
                 <div className="text-sm font-medium text-gray-900">
                   {`Already have account? `}
